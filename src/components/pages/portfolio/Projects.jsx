f6d9e77671a6304da05969c01fed9290779cd396
DESKTOP-JQ1EBN4\Asus <ankeara017@gmail.com>
2024-09-23 18:28:34 +0700

    main

--- src/components/pages/portfolio/Projects.jsx
+++ src/components/pages/portfolio/Projects.jsx
@@ -1,8 +1,11 @@
+import { Link } from 'react-router-dom';
 
 const Projects = (props) => {
   return (
     <div className="project__items" >
+      <Link>
         <img src={props.image} alt="" className="img__project" />
+    </Link>
         <span className="project__title">{props.title}</span>
     </div>
   );
