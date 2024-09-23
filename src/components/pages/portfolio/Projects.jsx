77b703582cf5952aaa7a47112a829ded7ff17a4a
DESKTOP-JQ1EBN4\Asus <ankeara017@gmail.com>
2024-09-23 19:50:05 +0700

    again

--- src/components/pages/portfolio/Projects.jsx
+++ src/components/pages/portfolio/Projects.jsx
@@ -1,12 +1,21 @@
-import { Link } from 'react-router-dom';
+import { Figma } from 'lucide-react';
+import { Github } from 'lucide-react';
 
 const Projects = (props) => {
   return (
     <div className="project__items" >
-      <Link>
+      <a href="">
         <img src={props.image} alt="" className="img__project" />
-    </Link>
+    </a>
         <span className="project__title">{props.title}</span>
+<div className="icon_link">
+  <div className="link">
+    <Figma />
+  </div>
+  <div className="link">
+    <Github />
+  </div>
+</div>
     </div>
   );
 };
