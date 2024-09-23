0e53581227657df155a766473db6708898dfeffb
DESKTOP-JQ1EBN4\Asus <ankeara017@gmail.com>
2024-09-24 00:02:50 +0700

    add image with some code

--- src/components/pages/portfolio/Portfolio.jsx
+++ src/components/pages/portfolio/Portfolio.jsx
@@ -89,7 +89,7 @@ const Portfolio = () => {
             <h1 className="title">I Make the <span>best Our Portfolio</span></h1>
             <div className="project__demo grid">
               {currentPosts.map((data) => (
-                <Projects key={data.id} image={data.image} title={data.title} />
+                <Projects key={data.id} image={data.image} title={data.title} figma={data.figma} github={data.github}/>
               ))}
             </div>
             <Pagination
--- src/components/pages/portfolio/Projects.jsx
+++ src/components/pages/portfolio/Projects.jsx
@@ -4,16 +4,18 @@ import { Github } from 'lucide-react';
 const Projects = (props) => {
   return (
     <div className="project__items" >
-        <img src={props.image} alt="" className="img__project" />
+        <div className="box__img">
+          <img src={props.image} alt="" className="img__project" />
+          <div className="icon_link">
+            <a href={props.figma} className="link">
+              <Figma />
+            </a>
+            <a href={props.github} className="link">
+              <Github />
+            </a>
+          </div>
+        </div>
         <span className="project__title">{props.title}</span>
-      <div className="icon_link">
-        <a href={props.figma} className="link">
-          <Figma />
-        </a>
-        <a href={props.github} className="link">
-          <Github />
-        </a>
-      </div>
     </div>
   );
 };
